11096fd8ca543da58e8f165b1301c811e308a3ca
Zach Sheley <zsheley@iu.edu>
2020-04-23 20:43:42 +0000

    Finished 6: Lock Code (parts 1-3)

--- code/stolen_artifacts.py
+++ code/stolen_artifacts.py
@@ -43,18 +43,71 @@ def population_count(year):
       if int(row[year]) > 2000000:
         country_count += 1
     return country_count
+  
+def country_index(country):
+  file = 'code/world_population.csv'
+  with open(file) as csv_file:
+    world_population = csv.DictReader(csv_file, delimiter=',')
+    country_list = []
+    for row in world_population:
+      country_list.append(row['Country Name'])
+    country_list.sort()
+    for item in country_list:
+      if item == country:
+        return country_list.index(country)
 
+def population_decrease_count(year1, year2):
+  file = 'code/world_population.csv'
+  with open(file) as csv_file:
+    world_population = csv.DictReader(csv_file, delimiter=',')
+    country_count = 0
+    for row in world_population:
+      if int(row[year1]) < int(row[year2]):
+        country_count += 1
+    return country_count
 
+def world_cup_winner(year):
+  file = 'code/world_cup.csv'
+  with open(file) as csv_file:
+    world_cup = csv.DictReader(csv_file, delimiter=',')
+    for row in world_cup:
+      if row['year'] == year:
+        return row['winner_code']
+
+def airport_type_count(airport_type, country_code):
+  file = 'code/airport_codes.csv'
+  with open(file) as csv_file:
+    airport_codes = csv.DictReader(csv_file, delimiter=',')
+    airport_count = 0
+    for row in airport_codes:
+      if row['type'] == airport_type and row['iso_country'] == country_code:
+        airport_count += 1
+    return airport_count
+
+  
+  
+  
 def main():
   # 1: Finding the Airport Variables
-  world_cup_country = 'South Africa'
-  country = 'Guatemala'
+  world_cup_country = 'England' # 1: South Africa 2: England
+  country = 'Uzbekistan' # 1: Guatemala 2: Uzbekistan
   
   # 2: Locker Number (part 1) Variables
-  country_last_letter = 'i'
+  country_last_letter = 'd' # 1: i 2: d
   
   # 3: Locker Number (part 2) Variables
-  country_populations_year = '2000'
+  country_populations_year = '1975' # 1: 2000 2: 1975
+  
+  # 4: Lock Code (part 1) Variables
+  country_name = 'Austria' # 1: Azerbaijan 2: Austria
+  
+  # 5: Lock Code (part 2) Variables
+  population_year = '1991' # 1: 2010 2: 1991
+  population_year_2 = '1968' # 1: 2000 2: 1968
+  
+  # 6: Lock Code (part 3) Variables
+  world_cup_year = '1978' # 1: 2010 2: 1978
+  airport_type = 'heliport' # 1 & 2: heliport
   
   # 1: Finding the Airport
   print(airport_code(population(country, host_year(world_cup_country))))
@@ -62,7 +115,9 @@ def main():
   # 2: Locker Number (part 1) and 3: Locker Number (part 2)
   print(f"{country_code(country_last_letter)}{population_count(country_populations_year)}")
   
-
+  # 4: Lock Code (part 1) and 5: Lock Code (part 2) and 6: Lock Code (part 3)
+  print(f"{country_index(country_name)} {population_decrease_count(population_year, population_year_2)} {airport_type_count(airport_type, world_cup_winner(world_cup_year))}")
+  
   
 if __name__== "__main__":
   main()
