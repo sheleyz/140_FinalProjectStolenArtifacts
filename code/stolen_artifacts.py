cd945a6b2368721066bc05d321750a1f4b7410fc
Zach Sheley <zsheley@iu.edu>
2020-04-23 17:16:18 +0000

    Finished Locker Number (part 1)

--- code/stolen_artifacts.py
+++ code/stolen_artifacts.py
@@ -22,18 +22,31 @@ def airport_code(population):
   with open(file) as csv_file:
     airport_code = csv.DictReader(csv_file, delimiter=',')
     for row in airport_code:
-      if row['latitude'] == '147.466003':
+      if row['latitude'] == str(latitude):
         return row['iata_code']
-# if row['latitude'] == latitude:
+
+def country_code(letter):
+  file = 'code/world_population.csv'
+  # old code file needs: (file, encoding="utf-8-sig")
+  with open(file) as csv_file:
+    world_population = csv.DictReader(csv_file, delimiter=',')
+    for row in world_population:
+      if row['Country Name'][-1] == letter:
+        return row['Country Code']
+
 
 def main():
   # 1: Finding the Airport Variables
   world_cup_country = 'South Africa'
   country = 'Guatemala'
   
+  country_last_letter = 'i'
+  
   # 1: Finding the Airport
   print(airport_code(population(country, host_year(world_cup_country))))
 
+  # 2: Locker Number (part 1)
+  print(country_code(country_last_letter))
   
 if __name__== "__main__":
   main()
